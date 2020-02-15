6c37e89d8e1525c4ef8304522a0e62b975dd76d3
EmirVelazquez <emirleovelazquez@gmail.com>
2020-02-15 13:22:48 -0600

    fixes to index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app => need to set to `build` instead of `public` after hosting
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
